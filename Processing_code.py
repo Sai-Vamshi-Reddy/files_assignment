5d0333b656b30190a60964127f63f03d536953d8
Sai-Vamshi-Reddy <skethireddy@murraystate.edu>
2024-10-20 09:49:34 -0500

    data to csv file

--- Processing_code.py
+++ Processing_code.py
@@ -17,3 +17,31 @@ def open_json_file(file_path="sai_vamshi_adoptions.json"):
         print("Error reading JSON. Check the file format.")
     except Exception as e:
         print("An error occurred:", e) 
+
+# Function to save the adoption data to a CSV file
+def save_universities_to_csv(data):
+    if data is None:
+        return
+
+    file = open('sai_vamshi_universities.csv', mode='w', newline='')
+    writer = csv.writer(file)
+    writer.writerow(['University ID', 'University Name', 'Address', 'City', 'State', 'Website', 'Zip','Longitude','Latitude','Classification'])
+
+    for record in data:
+        university = record['university']
+        writer.writerow([
+            university['id'],
+            university['name'],
+            university.get('address', 'N/A'),
+            university['city'],
+            university['state'],
+            university.get('website', 'N/A'),
+            university['zip'],
+            university['longitude'],
+            university['latitude'],
+            university['classification']
+        ])
+
+    file.close()
+    print("University data saved to CSV.")
+ 
